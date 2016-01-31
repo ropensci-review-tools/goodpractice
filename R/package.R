a694c39f330bc4b53ada60993a0723ee7221f8d6
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-31 21:06:06 +0000

    Fixes after Mason

--- R/package.R
+++ R/package.R
@@ -2,8 +2,8 @@
 #' Advice on R Package Building
 #'
 #' Give advice about good practices when building R packages. Advice includes
-  functions and syntax to avoid, package structure, code complexity, code
-  formatting, etc.
+#' functions and syntax to avoid, package structure, code complexity, code
+#' formatting, etc.
 #'
 #' @docType package
 #' @name goodpractice

3a3c1046bada9f7fcad54ad69faa684ca5efff5e
Gábor Csárdi <csardi.gabor@gmail.com>
2016-06-25 12:23:12 +0100

    Use crayon to color the output

--- R/print.R
+++ R/print.R
@@ -57,13 +57,13 @@ header_line <- function(left = "", right = "",
 
 gp_header <- function(x) {
   h <- header_line(left = paste("GP", x$package))
-  cat(h, "\n\n", sep = "")
-  cat("It is good practice to\n\n")
+  cat(crayon::yellow(h), "\n\n", sep = "")
+  cat(crayon::bold("It is good practice to"), "\n\n", sep = "")
 }
 
 gp_footer <- function(x) {
   f <- header_line()
-  cat(f, "\n")
+  cat(crayon::yellow(f), "\n")
 }
 
 #' @importFrom clisymbols symbol
@@ -77,7 +77,11 @@ gp_advice <- function(state, fail) {
 
   str <- gsub("\n\\s*", " ", str)
   str <- paste(
-    strwrap(paste0(symbol$cross, " ", str), indent = 2, exdent = 4),
+    strwrap(
+      paste0(crayon::red(symbol$cross), " ", str),
+      indent = 2,
+      exdent = 4
+    ),
     collapse = "\n"
   )
 
@@ -95,9 +99,13 @@ gp_positions <- function(pos, limit = 5) {
 
   cat("\n\n")
   lapply(pos, function(x) {
-    cat(sep = "", "    ", x$filename, ":", x$line_number, ":",
-        x$column_number, "\n")
+    cat(sep = "", "    ", crayon::blue(x$filename), ":",
+        crayon::blue(as.character(x$line_number)), ":",
+        crayon::blue(as.character(x$column_number)), "\n")
   })
 
-  if (num > limit) cat("    ... and ", num - 5, " more lines\n")
+  if (num > limit) {
+    and <- paste0("    ... and ", num - 5, " more lines\n")
+    cat(crayon::blue(and))
+  }
 }

342f62489c4177499ffa1a6bbc0b787b39e9aa32
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-21 13:46:39 +0100

    Turn off covr markers in RStudio, temporarily

--- R/prep_covr.R
+++ R/prep_covr.R
@@ -1,10 +1,14 @@
 
 #' @include lists.R
 #' @importFrom covr package_coverage zero_coverage
+#' @importFrom withr with_options
 
 PREPS$covr <- function(state, path = state$path) {
   covr <- list(coverage = package_coverage(path))
-  covr$zero <- zero_coverage(covr$coverage)
+  with_options(
+    list(covr.rstudio_source_markers = FALSE),
+    covr$zero <- zero_coverage(covr$coverage)
+  )
   state$covr <- covr
   state
 }

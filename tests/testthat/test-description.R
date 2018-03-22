0d9476cc03fbd1dcfc7590128ce270f9d4e6522d
Hannah Frick <hfrick@mango-solutions.com>
2018-03-22 21:45:31 +0000

    added tests for the remaining checks on description

--- tests/testthat/test-description.R
+++ tests/testthat/test-description.R
@@ -7,6 +7,8 @@ Title: Foo Package
 Maintainer: foo@foofoo.com
 Description: What the package does.
 Depends: R (>= 2.15)
+URL: https://www.foo.com
+BugReports: https://www.foo.com/bugs
 "
 
 D2 <-
@@ -15,6 +17,7 @@ Title: Foo Package
 Maintainer: foo@foofoo.com
 Description: What the package does.
 Depends: foobar (>= 2.15)
+Date: 2018-03-22
 "
 
 test_that("Depends: R is OK", {
@@ -27,5 +30,44 @@ test_that("Depends: R is OK", {
   state <- list(description = desc::description$new(text = D2))
   expect_false(
     CHECKS$no_description_depends$check(state)
+  )
+})
+
+test_that("Date", {
+  
+  state <- list(description = desc::description$new(text = D1))
+  expect_true(
+    CHECKS$no_description_date$check(state)
+  )
+  
+  state <- list(description = desc::description$new(text = D2))
+  expect_false(
+    CHECKS$no_description_date$check(state)
+  )
+})
+
+test_that("URL", {
+  
+  state <- list(description = desc::description$new(text = D1))
+  expect_true(
+    CHECKS$description_url$check(state)
+  )
+  
+  state <- list(description = desc::description$new(text = D2))
+  expect_false(
+    CHECKS$description_url$check(state)
+  )
+})
+
+test_that("BugReports", {
+  
+  state <- list(description = desc::description$new(text = D1))
+  expect_true(
+    CHECKS$description_bugreports$check(state)
+  )
+  
+  state <- list(description = desc::description$new(text = D2))
+  expect_false(
+    CHECKS$description_bugreports$check(state)
   )
 })

ea2dcc862baddfcfdee36e6cf6174ad4d8e03c78
Hannah Frick <hfrick@mango-solutions.com>
2018-04-23 16:06:53 +0100

    Revert "just added new line after warning in prep steps"
    This reverts commit 08116e6935d9d531ac11275155e523f9dcb0c1f1.

--- R/prep_description.R
+++ R/prep_description.R
@@ -4,6 +4,6 @@
 
 PREPS$description <- function(state, path = state$path, quiet) {
   state$description <- try(description$new(file.path(path, "DESCRIPTION")), silent = quiet)
-  if(inherits(state$description, "try-error")) warning("Prep step for description failed.\n")
+  if(inherits(state$description, "try-error")) warning("Prep step for description failed.")
   state
 }

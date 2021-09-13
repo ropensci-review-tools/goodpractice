e2245567bab00e13630d9834aac1a6c91b56eeb1
mpadge <mark.padgham@email.com>
2021-09-13 13:13:43 +0200

    add describe_check function

--- R/lists.R
+++ R/lists.R
@@ -10,3 +10,20 @@ CHECKS <- list()
 all_checks <- function() {
   names(CHECKS)
 }
+
+#' Describe one or more checks
+#'
+#' @return List of character descriptions for each \code{check_name}
+#' @export
+#' @examples 
+#' describe_check("rcmdcheck_non_portable_makevars")
+#' check_name <- c ("no_description_depends",
+#'                  "lintr_assignment_linter",
+#'                  "no_import_package_as_a_whole",
+#'                  "rcmdcheck_missing_docs")
+#' describe_check(check_name)
+
+describe_check <- function(check_name) {
+    check_name <- check_name[check_name %in% names(CHECKS)]
+    lapply(CHECKS[check_name], function(i) i$description)
+} 

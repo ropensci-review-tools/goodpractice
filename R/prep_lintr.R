9d2c52eb1e16618325733afe429560f65253d133
owenjonesuob <joneso21164@gmail.com>
2022-07-12 23:53:22 +0100

    Switch to function-factory-linter approach from lintr 3.0.0

--- R/prep_lintr.R
+++ R/prep_lintr.R
@@ -2,14 +2,14 @@
 #' @include my_linters.R
 
 linters_to_lint <- list(
-  assignment_linter = lintr::assignment_linter,
+  assignment_linter = lintr::assignment_linter(),
   line_length_linter = lintr::line_length_linter(80),
-  trailing_semicolon_linter = trailing_semicolon_linter,
-  attach_detach_linter = attach_detach_linter,
-  setwd_linter = setwd_linter,
-  sapply_linter = sapply_linter,
-  library_require_linter = library_require_linter,
-  seq_linter = seq_linter
+  trailing_semicolon_linter = trailing_semicolon_linter(),
+  attach_detach_linter = attach_detach_linter(),
+  setwd_linter = setwd_linter(),
+  sapply_linter = sapply_linter(),
+  library_require_linter = library_require_linter(),
+  seq_linter = seq_linter()
 )
 
 #' @include lists.R
@@ -18,7 +18,7 @@ linters_to_lint <- list(
 PREPS$lintr <- function(state, path = state$path, quiet) {
   path <- normalizePath(path)
   suppressMessages(
-    state$lintr <- try(lint_package(path, linters = linters_to_lint), 
+    state$lintr <- try(lint_package(path, linters = linters_to_lint),
                        silent = TRUE)
   )
   if(inherits(state$lintr, "try-error")) {

6283afc8592b64800cf258e9dca574ffd658bc92
mpadge <mark.padgham@email.com>
2024-09-25 14:31:08 +0200

    rm 'dangerous_fn_linter' for #173

--- R/prep_lintr.R
+++ R/prep_lintr.R
@@ -5,10 +5,20 @@ linters_to_lint <- list(
   assignment_linter = lintr::assignment_linter(),
   line_length_linter = lintr::line_length_linter(80),
   trailing_semicolon_linter = trailing_semicolon_linter(),
-  attach_detach_linter = attach_detach_linter(),
-  setwd_linter = setwd_linter(),
-  sapply_linter = sapply_linter(),
-  library_require_linter = library_require_linter(),
+  attach_detach_linter = lintr::undesirable_function_linter(fun = c(
+    "attach" = "Avoid attach, it is easy to create errors with it",
+    "detach" = "Avoid detach, it is easy to create errors with it"
+  )),
+  setwd_linter = lintr::undesirable_function_linter(fun = c(
+    "setwd" = "Avoid changing the working directory, or restore it in on.exit"
+  )),
+  sapply_linter = lintr::undesirable_function_linter(fun = c(
+    "sapply" = "Avoid using sapply, consider vapply instead, that's type safe"
+  )),
+  library_require_linter = lintr::undesirable_function_linter(fun = c(
+    "library" = "Avoid library() calls in packages",
+    "require" = "Avoid require() calls in packages"
+  )),
   seq_linter = seq_linter()
 )
 

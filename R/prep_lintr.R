9083c9f8657d1f318e2db18d137ccbcee0c696d8
mpadge <mark.padgham@email.com>
2024-09-26 12:07:58 +0200

    use lintr default undesirable_function defs for #173

--- R/prep_lintr.R
+++ R/prep_lintr.R
@@ -5,20 +5,18 @@ linters_to_lint <- list(
   assignment_linter = lintr::assignment_linter(),
   line_length_linter = lintr::line_length_linter(80),
   trailing_semicolon_linter = trailing_semicolon_linter(),
-  attach_detach_linter = lintr::undesirable_function_linter(fun = c(
-    "attach" = "Avoid attach, it is easy to create errors with it",
-    "detach" = "Avoid detach, it is easy to create errors with it"
-  )),
-  setwd_linter = lintr::undesirable_function_linter(fun = c(
-    "setwd" = "Avoid changing the working directory, or restore it in on.exit"
-  )),
-  sapply_linter = lintr::undesirable_function_linter(fun = c(
-    "sapply" = "Avoid using sapply, consider vapply instead, that's type safe"
-  )),
-  library_require_linter = lintr::undesirable_function_linter(fun = c(
-    "library" = "Avoid library() calls in packages",
-    "require" = "Avoid require() calls in packages"
-  )),
+  attach_detach_linter = lintr::undesirable_function_linter(
+    fun = lintr::default_undesirable_functions[c("attach", "detach")]
+  ),
+  setwd_linter = lintr::undesirable_function_linter(
+    fun = lintr::default_undesirable_functions["setwd"]
+  ),
+  sapply_linter = lintr::undesirable_function_linter(
+    fun = lintr::default_undesirable_functions["sapply"]
+  ),
+  library_require_linter = lintr::undesirable_function_linter(
+    fun = lintr::default_undesirable_functions[c("library", "require")]
+  ),
   seq_linter = seq_linter()
 )
 

34e55934afbcbf21278393b0e9d22f0ea8198ec6
mpadge <mark.padgham@email.com>
2025-06-24 09:57:52 +0200

    improve test-describe-check

--- tests/testthat/test-describe-check.R
+++ tests/testthat/test-describe-check.R
@@ -1,27 +1,25 @@
-
-context("describe check")
-
 test_that("describe_check", {
-    expect_length(describe_check(), 0L)
-    chk <- "rcmdcheck_non_portable_makevars"
-    d <- describe_check(chk)
-    expect_is(d, "list")
-    expect_length(d, length(chk))
-    expect_named(d, chk)
+  expect_length(describe_check(), 0L)
+  chk1 <- "rcmdcheck_non_portable_makevars"
+  d1 <- describe_check(chk1)
+  expect_type(d1, "list")
+  expect_length(d1, length(chk1))
+  expect_named(d1, chk1)
 
-    chk <- c("no_description_depends",
-             "lintr_assignment_linter",
-             "no_import_package_as_a_whole",
-             "rcmdcheck_missing_docs")
-    d <- describe_check(chk)
-    expect_is(d, "list")
-    expect_length(d, length(chk))
-    expect_named(d, chk)
+  chks4 <- c(
+    "no_description_depends",
+    "lintr_assignment_linter",
+    "no_import_package_as_a_whole",
+    "rcmdcheck_missing_docs"
+  )
+  d4 <- describe_check(chks4)
+  expect_type(d4, "list")
+  expect_length(d4, length(chks4))
+  expect_named(d4, chks4)
 
-    chk <- c(chk, "a", "b") # "a", "b" are not checks, so ignored
-    d <- describe_check(chk)
-    expect_is(d, "list")
-    expect_false(length(d) == length(chk))
-    expect_false(identical(chk, names(d)))
-    expect_lt(length(d), length(chk))
+  chks6 <- c(chks4, "a", "b") # "a", "b" are not checks, so ignored
+  d6 <- describe_check(chks6)
+  expect_type(d6, "list")
+  expect_false(length(d6) == length(chks6))
+  expect_identical(d6, d4)
 })

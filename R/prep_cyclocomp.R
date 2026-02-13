dd17720f45ba72ad0e6db3140220d20388f68603
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2026-02-13 12:25:57 +0100

    run in callr process to silecne build

--- R/prep_cyclocomp.R
+++ R/prep_cyclocomp.R
@@ -3,8 +3,19 @@
 #' @importFrom cyclocomp cyclocomp_package_dir
 
 PREPS$cyclocomp <- function(state, path = state$path, quiet) {
-  state$cyclocomp <- try(cyclocomp_package_dir(path), silent = quiet)
-  if(inherits(state$cyclocomp, "try-error")) {
+  if (quiet && requireNamespace("callr", quietly = TRUE)) {
+    state$cyclocomp <- try(
+      callr::r(
+        function(path) cyclocomp::cyclocomp_package_dir(path),
+        args = list(path = normalizePath(path)),
+        show = FALSE
+      ),
+      silent = TRUE
+    )
+  } else {
+    state$cyclocomp <- try(cyclocomp_package_dir(path), silent = quiet)
+  }
+  if (inherits(state$cyclocomp, "try-error")) {
     warning("Prep step for cyclomatic complexity failed.")
   }
   state

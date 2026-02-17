e88e732825dea6e57204866492a2f917c7f69f20
mpadge <mark.padgham@email.com>
2026-02-17 08:51:18 +0100

    activate proper cyclocompe prep quiet param
    https://github.com/gaborcsardi/cyclocomp/pull/28

--- R/prep_cyclocomp.R
+++ R/prep_cyclocomp.R
@@ -3,7 +3,7 @@
 #' @importFrom cyclocomp cyclocomp_package_dir
 
 PREPS$cyclocomp <- function(state, path = state$path, quiet) {
-  state$cyclocomp <- try(cyclocomp_package_dir(path), silent = quiet)
+  state$cyclocomp <- try(cyclocomp_package_dir(path, quiet = quiet))
   if(inherits(state$cyclocomp, "try-error")) {
     warning("Prep step for cyclomatic complexity failed.")
   }

42882e2354c748875a813b875b3eeeb911ea4608
mpadge <mark.padgham@email.com>
2026-02-20 10:50:34 +0100

    restore 'silent = quiet' as well in cyclocomp prep; thanks @maelle

--- R/prep_cyclocomp.R
+++ R/prep_cyclocomp.R
@@ -3,7 +3,7 @@
 #' @importFrom cyclocomp cyclocomp_package_dir
 
 PREPS$cyclocomp <- function(state, path = state$path, quiet) {
-  state$cyclocomp <- try(cyclocomp_package_dir(path, quiet = quiet))
+  state$cyclocomp <- try(cyclocomp_package_dir(path, quiet = quiet), silent = quiet)
   if(inherits(state$cyclocomp, "try-error")) {
     warning("Prep step for cyclomatic complexity failed.")
   }

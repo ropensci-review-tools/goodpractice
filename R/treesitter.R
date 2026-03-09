a34f532e47f09525a16f183e6656bff525ebb430
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2026-03-09 21:22:44 +0100

    fix double ntested loop

--- R/treesitter.R
+++ R/treesitter.R
@@ -25,6 +25,27 @@ ts_body_has_call <- function(fn_node, call_query) {
   FALSE
 }
 
+ts_file_functions <- function(root, file) {
+  n_children <- treesitter::node_child_count(root)
+  fns <- list()
+  for (i in seq_len(n_children)) {
+    child <- treesitter::node_child(root, i)
+    if (treesitter::node_type(child) != "binary_operator") next
+    lhs <- treesitter::node_child_by_field_name(child, "lhs")
+    rhs <- treesitter::node_child_by_field_name(child, "rhs")
+    if (is.null(rhs)) next
+    if (treesitter::node_type(rhs) != "function_definition") next
+    if (treesitter::node_type(lhs) != "identifier") next
+    fns[[length(fns) + 1]] <- list(
+      name = treesitter::node_text(lhs),
+      file = file,
+      line = treesitter::node_start_point(lhs)$row + 1L,
+      fn_node = rhs
+    )
+  }
+  fns
+}
+
 ts_parse <- function(path) {
   rdir <- file.path(path, "R")
   if (!dir.exists(rdir)) {
@@ -33,11 +54,10 @@ ts_parse <- function(path) {
 
   lang <- treesitter.r::language()
   p <- treesitter::parser(lang)
-
   rfiles <- list.files(rdir, pattern = "\\.[rR]$", full.names = TRUE)
+
   trees <- list()
   functions <- list()
-
   for (f in rfiles) {
     code <- tryCatch(
       paste(readLines(f, warn = FALSE), collapse = "\n"),
@@ -48,24 +68,7 @@ ts_parse <- function(path) {
     tree <- treesitter::parser_parse(p, code)
     root <- treesitter::tree_root_node(tree)
     trees[[f]] <- list(tree = tree, root = root)
-
-    n_children <- treesitter::node_child_count(root)
-    for (i in seq_len(n_children)) {
-      child <- treesitter::node_child(root, i)
-      if (treesitter::node_type(child) != "binary_operator") next
-      lhs <- treesitter::node_child_by_field_name(child, "lhs")
-      rhs <- treesitter::node_child_by_field_name(child, "rhs")
-      if (is.null(rhs)) next
-      if (treesitter::node_type(rhs) != "function_definition") next
-      if (treesitter::node_type(lhs) != "identifier") next
-
-      functions[[length(functions) + 1]] <- list(
-        name = treesitter::node_text(lhs),
-        file = f,
-        line = treesitter::node_start_point(lhs)$row + 1L,
-        fn_node = rhs
-      )
-    }
+    functions <- c(functions, ts_file_functions(root, f))
   }
 
   list(trees = trees, functions = functions, language = lang)

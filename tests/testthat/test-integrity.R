393549be677217a5eb8fd31ea1d9011ceae0728d
mpadge <mark.padgham@email.com>
2026-07-23 13:07:30 +0200

    fix test-integrity.R
    That test only every worked on local pkg source. Elsewhere, including
    GitHub runners, the path '..', '..', 'R' leads to installed '.rdb'
    binary files, which throw 1,000s of warnings trying to read those as
    text files on GitHub test runners. This fix ensures only actual
    files are read, and also informatively skips if there are none of those.

--- tests/testthat/test-integrity.R
+++ tests/testthat/test-integrity.R
@@ -2,7 +2,8 @@ test_that("checks are not overwritten", {
   skip_on_cran()
 
   pkgdir <- file.path("..", "..", "R")
-  rfiles <- list.files(pkgdir, full.names = TRUE)
+  rfiles <- list.files(pkgdir, full.names = TRUE, pattern = "\\.R$")
+  skip_if(length(rfiles) == 0, "no R source files found (not running against source tree)")
   rlines <- as.character(unlist(lapply(rfiles, readLines)))
   rwords <- unlist(strsplit(rlines, "\\s+"))
   checks <- grep("CHECKS$", rwords, fixed = TRUE, value = TRUE)
